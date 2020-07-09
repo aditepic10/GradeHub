9a481c3a14cbe9899bf588289ce6f839e3accf57
Adit Pareek <aditepic10@gmail.com>
2020-07-09 09:54:15 -0400

    Update train.py with two column output

--- src/T2-Student_Progress_Tracking/train.py
+++ src/T2-Student_Progress_Tracking/train.py
@@ -20,24 +20,28 @@ for i in category_colums:
     df[i] = enc.fit_transform(df[i])
 
 X = df.drop(['school', 'G1', 'G2'], axis=1)  # Remove all grades except for final
-y = df['G3']
-sorted_y = sorted(y[i] for i in range(len(y)))
+y = pd.DataFrame(df["G3"], columns=["G3"])  # first = at_risk? second = not_at_risk
+sorted_y = sorted(y["G3"][i] for i in range(len(y)))
+y2_helper = []
 for i in range(len(y)):
-    orig = y[i]  # save original value
+    orig = y["G3"][i]  # save original value
     # at-risk defined as percentile < 0.10 (tent.)
-    y.loc[i] = 1 if (sorted_y.index(y[i]) / len(sorted_y)) < 0.10 else 0
+    y["G3"].loc[i] = 1 if (sorted_y.index(y["G3"][i]) / len(sorted_y)) < 0.10 else 0  # 1 = at_risk
+    y2_helper.append(0 if (y["G3"].loc[i] == 1) else 1)  # binary values; opposite of at_risk
     sorted_y.remove(orig)  # remove original value from sorted list
 
 used_data = {"failures", "Medu", "higher", "age", "Fedu", "goout", "romantic"}  # Labels to be used
 X = X.drop([label for label in X if label not in used_data], axis=1)  # remove all irrelevant labels
-
+y["G3-2"] = y2_helper  # add new label; complement of first
+# print(y.head)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)  # split train/test data
 sc = StandardScaler()  # scale to transform data for neural network
 X_train = sc.fit_transform(X_train)  # transform data to be used in neural network
 X_test = sc.transform(X_test)  # transform data to be used in neural network
 
-architecture = (1000, 500, 250, 125, 500, 750)  # replace with any architecture
-model = MLPClassifier(solver='lbfgs', learning_rate="adaptive", hidden_layer_sizes=architecture, random_state=1, max_iter=100000000)
+architecture = (198,)  # replace with any architecture
+model = MLPClassifier(solver='lbfgs', learning_rate="adaptive", hidden_layer_sizes=architecture, random_state=1,
+                      max_iter=100000000)
 # model = DecisionTreeClassifier()  # Decision tree model
 
 print("Preprocessing time: %ss" % (round(time.perf_counter() - t_start, 3)))  # time to preprocess
